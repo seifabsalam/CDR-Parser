1d1c2756c25a548a60293cdca98bca56c70b9731
seifabsalam <seif.absalam@gmail.com>
2026-06-22 18:28:43 +0300

    Feat: adding database writer

---
+++ src/main/java/com/billing/cdrparser/DatabaseWriter.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.billing.cdrparser;
+
+import com.telecom.billing.db.tables.*;
+import com.telecom.billing.db.tables.records.*;
+import java.math.BigDecimal;
+import java.util.List;
+import org.jooq.DSLContext;
+import org.jooq.InsertSetMoreStep;
+
+/**
+ *
+ * @author seif
+ */
+public class DatabaseWriter {
+    private static final Cdr CDR = Cdr.CDR;
+    
+    public static boolean write(List<LineItem> items, DSLContext ctx){
+        
+        for(LineItem item:items){
+                ctx.insertInto(CDR)
+                    .set(CDR.CONTRACT_ID,item.getContractId())
+                    .set(CDR.DIAL_A, item.getDialA())
+                    .set(CDR.DIAL_B, item.getDialB())
+                    .set(CDR.DURATION, item.getDuration())
+                    .set(CDR.EXTERNAL_FEE_PIASTERS, item.getExternalFeePiasters())
+                    .set(CDR.SERVICE_TYPE, item.getServiceType())
+                    .set(CDR.START_TIME, item.getStartTime())
+                    .set(CDR.IS_RATED, item.isRated());
+        }
+        return true;
+    }
+}
