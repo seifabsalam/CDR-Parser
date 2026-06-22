32a16e22abdbff41eb904188aee8e79ccafb79c7
seifabsalam <seif.absalam@gmail.com>
2026-06-22 18:37:05 +0300

    fix: DatabaseWriter code review fixes
    - Add .execute() so inserts are actually sent to the database
    - Change return type from misleading boolean to void
    - Remove NetBeans boilerplate template comments and @author tag

--- src/main/java/com/billing/cdrparser/DatabaseWriter.java
+++ src/main/java/com/billing/cdrparser/DatabaseWriter.java
@@ -1,33 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.billing.cdrparser;
 
 import com.telecom.billing.db.tables.*;
 import java.util.List;
 import org.jooq.DSLContext;
 
-/**
- *
- * @author seif
- */
 public class DatabaseWriter {
     private static final Cdr CDR = Cdr.CDR;
-    
-    public static boolean write(List<LineItem> items, DSLContext ctx){
-        
-        for(LineItem item:items){
-                ctx.insertInto(CDR)
-                    .set(CDR.CONTRACT_ID,item.getContractId())
+
+    public static void write(List<LineItem> items, DSLContext ctx) {
+        for (LineItem item : items) {
+            ctx.insertInto(CDR)
+                    .set(CDR.CONTRACT_ID, item.getContractId())
                     .set(CDR.DIAL_A, item.getDialA())
                     .set(CDR.DIAL_B, item.getDialB())
                     .set(CDR.DURATION, item.getDuration())
                     .set(CDR.EXTERNAL_FEE_PIASTERS, item.getExternalFeePiasters())
                     .set(CDR.SERVICE_TYPE, item.getServiceType())
                     .set(CDR.START_TIME, item.getStartTime())
-                    .set(CDR.IS_RATED, item.isRated());
+                    .set(CDR.IS_RATED, item.isRated())
+                    .execute();
         }
-        return true;
     }
 }
