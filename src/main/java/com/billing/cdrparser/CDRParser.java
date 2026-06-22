0ef2fda387540c948c1641ce26e49916d8883730
seifabsalam <seif.absalam@gmail.com>
2026-06-22 12:35:12 +0300

    Initial commit: CDR Parser Maven project

---
+++ src/main/java/com/billing/cdrparser/CDRParser.java
@@ -0,0 +1,13 @@
+package com.billing.cdrparser;
+
+
+/**
+ *
+ * @author seif
+ */
+public class CDRParser {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
